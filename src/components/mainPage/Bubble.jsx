fbab7bdb64a44e8e75ad60d41e5ab741cddf0eec
liliansby <sojadia@gmail.com>
2024-03-10 17:27:56 +0200

    made changes to the size of bubble

--- src/components/mainPage/Bubble.jsx
+++ src/components/mainPage/Bubble.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Bubble = () => {
   return (
-    <div className="w-[65px] h-[60px] bg-earthyOlive rounded-full mb-[-40px]" />
+    <div className="w-[67px] h-[63px] bg-earthyOlive rounded-full mb-[-40px]" />
   );
 };
 
