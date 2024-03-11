40dfbb18f7ea2fa769aeac8ddf83199e612eec9c
liliansby <sojadia@gmail.com>
2024-03-11 12:09:58 +0200

    review text changed

--- src/components/mainPage/ReviewsScreen.jsx
+++ src/components/mainPage/ReviewsScreen.jsx
@@ -19,6 +19,7 @@ const ReviewsScreen = () => {
     const div = document.getElementById("scrollableReviews");
     div.scrollLeft = div.scrollLeft + 400;
   };
+
   return (
     <div className="bg-mossySage h-[400px] flex">
       <button
@@ -35,38 +36,39 @@ const ReviewsScreen = () => {
         >
           <div className="flex w-fit justify-center items-center space-x-2 ">
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="This hair serum worked wonders! Silky-smooth locks with just a few drops. Love it!"
+              name="Olivia Bennett"
               image={img1}
             />
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
-              image={img2}
-            />
-            <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="I've struggled with frizzy hair for years until I found this smoothing cream. It's like magic in a bottle! It controls frizz even in humid weather, leaving my hair sleek and manageable. Plus, it doesn't weigh down my curls."
+              name="Mason Carter"
               image={img3}
             />
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="As someone with sensitive scalp, finding a shampoo that soothes without stripping natural oils is a challenge. This one is a gem. Gentle yet effective."
+              name="Isabella Thompson"
+              image={img2}
+            />
+
+            <ReviewCard
+              text="I have curly hair and this conditioner is a game-changer. Defines my curls without leaving them crunchy. Bye-bye frizz!"
+              name="Elijah Rodriguez"
               image={img4}
             />
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="Life-changing! This leave-in conditioner saved my damaged hair. It's lightweight, non-greasy, and adds the perfect amount of hydration. Highly recommend!"
+              name="Liam Anderson"
               image={img5}
             />
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="Finally found a product that tames my unruly hair! This styling gel provides long-lasting hold without stiffness. Plus, it smells fantastic. Highly recommend!"
+              name="Ava Wright"
               image={img6}
             />
             <ReviewCard
-              text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum, adipisci. "
-              name="name"
+              text="Five stars! This shampoo gave my hair incredible volume and shine. Plus, it smells amazing. My new go-to for sure!"
+              name="Noah Martinez"
               image={img7}
             />
           </div>
